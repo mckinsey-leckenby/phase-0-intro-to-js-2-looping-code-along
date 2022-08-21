a9d732b21ce0281cb545f196bb73a2865c194485
McKinsey Leckenby <91347096+mckinsey-leckenby@users.noreply.github.com>
2022-08-21 09:33:57 -0400

    commit tree ce785a9ee36d7012b0a01d9e052b78edb57e7310

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
-const name = ["Lisa", "Kaitlin", "Jan"];
-const surprise = "surprise";
 
-function writeCards(name, surprise) {
-    let messageArray =[];
-    for (let i=0; i < name.length; i++) {
-        let message = `Thank you, ${name[i]}, for the wonderful ${surprise} gift!`
-        messageArray.push(message);
-    }
-    return messageArray;
+function writeCards(names, surprise) {
+    let thankYouCards = [] 
+
+    
 }
 
-function countDown(int) {
-    let i=10;
-    while (i >= 0) {
-        console.log (i);
-        i--
+
+function countDown(begNum){
+    while(begNum > 0){
+        begNum -= 1
     }
-    return int;
+    console.log(begNum)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const name = ["Lisa", "Kaitlin", "Jan"];
+// const surprise = "surprise";
+
+// function writeCards(name, surprise) {
+//     let messageArray =[];
+//     for (let i=0; i < name.length; i++) {
+//         let message = `Thank you, ${name[i]}, for the wonderful ${surprise} gift!`
+//         messageArray.push(message);
+//     }
+//     return messageArray;
+// }
+
+// function countDown(int) {
+//     let i=10;
+//     while (i >= 0) {
+//         console.log (i);
+//         i--
+//     }
+//     return int;
+// }
